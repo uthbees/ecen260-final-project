ea7b4c7543e6b740cf2414d8a2df4fb66889bd23
Ethan Ball <uthbees@gmail.com>
2025-03-12 18:35:06 -0600

    Get basic esp32 program working

--- esp32-binary/README.md
+++ esp32-binary/README.md
@@ -8,3 +8,5 @@ Connect the board via USB, then run with cargo:
 ```bash
 cargo run
 ```
+
+You may need to do some troubleshooting. I had to use version 2.1.0 of `espflash`, as well as resetting my board before connecting to it by holding down the boot button, pressing the reset button, and releasing the boot button. (And then pressing the reset button again afterward to get the program to actually run.)
--- esp32-binary/src/main.rs
+++ esp32-binary/src/main.rs
@@ -1,5 +1,15 @@
-fn main() {
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::sys::EspError;
+
+fn main() -> Result<(), EspError> {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
+
+    let peripherals = Peripherals::take()?;
+    let mut led = PinDriver::output(peripherals.pins.gpio8)?; // A3
+    led.set_high()?;
+
+    loop {}
 }
