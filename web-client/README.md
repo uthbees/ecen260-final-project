f8e20e09dd5971e8923bd0bcd188f6f7b3fa37c2
Ethan Ball <uthbees@gmail.com>
2025-03-21 10:47:12 -0600

    Set up basic web server and web client programs

---
+++ web-client/README.md
@@ -0,0 +1,7 @@
+To run in development mode, run:
+
+```bash
+npm run dev
+```
+
+The client app can be built with `npm run build` and put on a web server if desired.
--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
